2ecd49c1e33432b88f9f008855055aa7017a1035
Mike S <soltys@colorado.edu>
2017-03-17 10:56:51 -0600

    Updated Comments

--- presTimer/presTimer.ino
+++ presTimer/presTimer.ino
@@ -1,40 +1,40 @@
 
 /*
-Large 7 Seg Timer
- By: Dr. Soltys
- University of Colorado
- Date: 3/6/2017
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- 
-Adapted from:
-Controlling large 7-segment displays
- By: Nathan Seidle
- SparkFun Electronics
- Date: February 25th, 2015
+  Large 7 Seg Timer
+  By: Dr. Soltys
+  University of Colorado
+  Date: 3/6/2017
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
 
-Here's how to hook up the Arduino pins to the Large Digit Driver IN
-Arduino 
- 6 -> CLK (Green on the 6-pin cable)
- 5 -> LAT (Blue)
- 7 -> SER on the IN side (Yellow)
- 5V -> 5V (Orange)
- Power Arduino with 12V and connect to Vin -> 12V (Red)
- GND -> GND (Black)
- 
-Additional hookups for buttons: one side is hooked up to D2 and D3, 
-with a 10kOhm pullup resistor connected to GND. 
-The other side is connected to 5V:
+  Adapted from:
+  Controlling large 7-segment displays
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: February 25th, 2015
 
- 2----10 kOhm - GND
+  Here's how to hook up the Arduino pins to the Large Digit Driver IN
+  Arduino
+  6 -> CLK (Green on the 6-pin cable)
+  5 -> LAT (Blue)
+  7 -> SER on the IN side (Yellow)
+  5V -> 5V (Orange)
+  Power Arduino with 12V and connect to Vin -> 12V (Red)
+  GND -> GND (Black)
+
+  Additional hookups for buttons: one side is hooked up to D2 and D3,
+  with a 10kOhm pullup resistor connected to GND.
+  The other side is connected to 5V:
+
+  2----10 kOhm - GND
     |
     Start button -- 5V
- 3----10 kOhm - GND
+  3----10 kOhm - GND
     |
     Stop button -- 5V
- 4----10 kOhm - GND
+  4----10 kOhm - GND
     |
-    Clear button -- 5V              
- */
+    Clear button -- 5V
+*/
 
 //GPIO declarations
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -46,15 +46,16 @@ byte segmentData = 7;
 //Timer Variables
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 volatile unsigned long startTime;
-volatile unsigned long stopTime=0;   
-volatile bool isRunning=false;
-#define timerError 0.001  //Might change with processor and manufacturer. Just do a test... run the timer for an hour with a stop watch next to it, and see !
-//note timerError should be positive for a clock that runs fast and neg. for a clock that runs slow.
+volatile unsigned long stopTime = 0;
+volatile bool isRunning = false;
+// This might change w/ board time and manufacturer, but this corrects for that error.
+// Use a negative value if timer runs slow.
+//  6 seconds / 4800 seconds = 0.00125
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 void setup()
 {
- // Serial.begin(9600);
+  // Serial.begin(9600);
   pinMode(segmentClock, OUTPUT);
   pinMode(segmentData, OUTPUT);
   pinMode(segmentLatch, OUTPUT);
@@ -64,61 +65,73 @@ void setup()
   digitalWrite(segmentLatch, LOW);
   showNumber(0); //Test pattern
 
-  pinMode(2,INPUT);
+  // Define Button Inputs
+  pinMode(2, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), startTimer, RISING);
-  pinMode(3,INPUT);
+  pinMode(3, INPUT);
   attachInterrupt(digitalPinToInterrupt(3), stopTimer, RISING);
-  pinMode(4,INPUT);
+  pinMode(4, INPUT);
 }
 
-
-
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// starTimer()
+// Interrupt function if timer button is pressed.
+// Starts the timer if it is currently stopped,
+// otherwise does nothing.
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void startTimer() {
-  if(!isRunning){
-   startTime=millis()-stopTime;
-   isRunning=true;
+  if (!isRunning) {
+    startTime = millis() - stopTime;
+    isRunning = true;
   }
 }
 
-
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// stopTimer()
+// Interrupt function if timer button is pressed.
+// pauses the timer if it is currently running,
+// otherwise does nothing.
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void stopTimer() {
-  if(isRunning){
-   isRunning=false;
-   stopTime=millis()-startTime;
+  if (isRunning) {
+    isRunning = false;
+    stopTime = millis() - startTime;
   }
 }
 
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// stopTimer()
+// clears the timer to zer0 of timer is stopped
+// otherwise does nothing.
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+void clearTimer() {
+  if (!isRunning) {
+    stopTime = 0;
+    showNumber(0);
+  }
+}
 
-void loop(){
-  if (isRunning){
-    unsigned long seconds=(millis()-startTime)/1000*(1-timerError);
-    unsigned int number=floor(seconds/60)*100+seconds%60;
-
-    showNumber(number); //Test pattern
+void loop() {
+  if (isRunning) {
+    unsigned long seconds = (millis() - startTime) / 1000 * (1 - timerError);
+    unsigned int number = floor(seconds / 60) * 100 + seconds % 60;
+    showNumber(number);
     //Serial.println(number); //For debugging
   }
-  else if (digitalRead(4)==HIGH){
-    stopTime=0;
-    showNumber(0);
-    }
+  else if (digitalRead(4) == HIGH) {
+    clearTimer();
+  }
 }
 
 //Takes a number and displays 2 numbers. Displays absolute value (no negatives)
-void showNumber(float value)
-{
+void showNumber(float value){
   int number = abs(value); //Remove negative signs and any decimals
 
-  Serial.print("number: ");
-  Serial.println(number);
-
-  for (byte x = 0 ; x < 4 ; x++)
-  {
+  for (byte x = 0 ; x < 4 ; x++){
     int remainder = number % 10;
 
-    if(x==2)
-      postNumber(remainder, true);
-    else
-      postNumber(remainder, false);
+    if (x == 2) postNumber(remainder, true);  //Decimil after 2nd digit
+    else        postNumber(remainder, false);
 
     number /= 10;
   }
@@ -129,8 +142,7 @@ void showNumber(float value)
 }
 
 //Given a number, or '-', shifts it out to the display
-void postNumber(byte number, boolean decimal)
-{
+void postNumber(byte number, boolean decimal){
   //    -  A
   //   / / F/B
   //    -  G
@@ -148,8 +160,7 @@ void postNumber(byte number, boolean decimal)
 
   byte segments;
 
-  switch (number)
-  {
+  switch (number) {
     case 1: segments = b | c; break;
     case 2: segments = a | b | d | e | g; break;
     case 3: segments = a | b | c | d | g; break;
@@ -168,8 +179,7 @@ void postNumber(byte number, boolean decimal)
   if (decimal) segments |= dp;
 
   //Clock these bits out to the drivers
-  for (byte x = 0 ; x < 8 ; x++)
-  {
+  for (byte x = 0 ; x < 8 ; x++){
     digitalWrite(segmentClock, LOW);
     digitalWrite(segmentData, segments & 1 << (7 - x));
     digitalWrite(segmentClock, HIGH); //Data transfers to the register on the rising edge of SRCK
